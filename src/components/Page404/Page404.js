fe70c47547558a3527979f498512658b553cb6a5
Citlalli DMG <citlallidmg@gmail.com>
2018-12-03 13:26:09 -0600

    establece rutas básicas

---
+++ src/components/Page404/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page404Page = () => {
+    return ( 
+        <div>
+            <h1>Page404</h1>
+        </div>
+    );
+}
+ 
+export default Page404Page;
---
+++ src/components/SignOut/SignOut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignOutPage = () => {
+    return ( 
+        <div>
+            <h1>SignOut Page</h1>
+        </div>
+    );
+}
+ 
+export default SignOutPage;
