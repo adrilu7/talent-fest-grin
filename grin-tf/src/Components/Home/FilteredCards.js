0d79dbba1447b7e9e54a6e4108f84f15220df8a9
florenciasilva <hensgrej@gmail.com>
2018-12-03 15:11:23 -0600

    Creación de clases con flexbox, maquetado de prueba

---
+++ grin-tf/src/Components/Home/FilteredCards.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../../App.css';
+
+const filteredCards = () => (
+    <div>
+        <div className='nav'>
+            <a href='#' className='nav-links'>Link 1</a>
+        </div>
+        <div className='container'>
+            <div className='card'>
+                <p>¡Holi!</p>
+            </div>
+
+            <div className='card'>
+                <p>¡Holi!</p>
+            </div>        
+            
+            <div className='card'>
+                <p>¡Holi!</p>
+            </div>
+
+        <div className='deck-container'>
+            <div className='deck-items'>
+                <p>holi dos</p>
+            </div>
+            <div className='deck-items'>
+                <p>holi dos</p>
+            </div>
+            <div className='deck-items'>
+                <p>holi dos</p>
+            </div>
+            <div className='deck-items'>
+                <p>holi dos</p>
+            </div>
+        </div>
+    </div>
+    </div>
+)
+
+export default filteredCards
