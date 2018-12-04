d6058e2f5c64b91cf7b07023fe83b4c3fdb70122
florenciasilva <hensgrej@gmail.com>
2018-12-04 02:22:49 -0600

    ?

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Row, Col} from 'react-materialize';
 import NavBar from './Navbar';
 
 const Home = () => (
     <div>
         <NavBar />
-        <div class="row with-padding">
-            <span class="col s12 m7 l7">This div is 12-columns wide on all screen sizes</span>
-            <span class="col s6 m3 l3">6-columns (offset-by-6)</span>
+        <div className="row with-padding">
+            <span className="col s12 m7 l7">This div is 12-columns wide on all screen sizes</span>
+            <span className="col s6 m3 l3">6-columns (offset-by-6)</span>
         </div>
     </div>        
 
