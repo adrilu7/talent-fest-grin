83853d0e8d62c7acd6f58aedae20691482f97565
florenciasilva <hensgrej@gmail.com>
2018-12-04 02:45:24 -0600

    Se agrega SignOut a NavBar

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Navbar, NavItem} from 'react-materialize';
-import logo from '../../assets/POKEDECK-NOMBRE.svg'
+import {Navbar, NavItem,} from 'react-materialize';
+import logo from '../../assets/POKEDECK-NOMBRE.svg';
+import SignOut from '../SignOut/SignOut';
 import './navBar.css'
 
 const navBar = () => (
     <Navbar className='nav-blue' right>
         <NavItem> <img src={logo} className='logo'/> </NavItem>
         <NavItem></NavItem>
-        <NavItem></NavItem>
+        <NavItem><SignOut/></NavItem>
     </Navbar>   
 )
 
